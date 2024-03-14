86be0acd45762f418a1b2f38a0d1db963aebb294
Maks6666 <maxim.kucher2005@gmail.com>
2024-03-14 11:01:20 +0200

    3rd homework task

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-# Створіть клас «Місто». Збережіть у класі: назву міста,
-# назву регіону, назву країни, кількість жителів у місті,
-# поштовий індекс міста, телефонний код міста. Реалізуйте
+# Створіть клас «Країна». Збережіть у класі: назву країни,
+# назву континенту, кількість жителів країни, телефонний
+# код країни, назву столиці, назву міст країни. Реалізуйте
 # методи класу для введення-виведення даних та інших
 # операцій.
 
-class City():
-    def __init__(self, name, region, state, population, phone_code):
+class State():
+    def __init__(self, name, continent, population, phone_code, capital, cities):
         self.name = name
-        self.region = region
-        self.state = state
+        self.continent = continent
         self.population = population
         self.phone_code = phone_code
-    def info_output(self):
-        print(f"City {self.name} located in region of {self.region} in {self.state}. Current population is: {self.population} and phone code is:'{self.phone_code}' ")
-    def info_input(self):
-        self.name = input("Input city name: ")
-        self.region = input("Input region name: ")
-        self.state = input("Input name of the state: ")
+        self.capital = capital
+        self.cities = cities
 
+    def info_output(self):
+        print(f"Country {self.name} located at {self.continent} with population of {self.population}. Phone code is: {self.phone_code}, capital city is {self.capital} and cities of country are: {self.cities}")
+    def info_input(self):
+        self.name = input("Input country name: ")
+        self.continent = input("Input continent: ")
         self.population = input("Input number of population: ")
         self.phone_code = input("Input phone code: ")
+        self.capital = input("Input capital city name: ")
+        self.cities = input("Input some cities: ")
+        print(f"Country {self.name} located at {self.continent} with population of {self.population}. Phone code is: {self.phone_code}, capital city is {self.capital} and cities of country are: {self.cities}")
 
-        print(
-            f"City {self.name} located in region of {self.region} in {self.state}. Current population is  {self.population} and phone code is  {self.phone_code}")
 
 
-
-city = City("Kyiv", "North", "Ukraine", "2.5M", "+380")
-city.info_output()
+state = State("Ukraine", "Europe", "40M", "+380", "Kiyv", "Kharkiv, Odessa, Lviv, Dnipro")
+state.info_input()
 
